f5dc161ac45877a4bbdc5e0c2859ff4928f70aed
Matt Pindell <pindell.m@gmail.com>
2016-06-28 13:42:05 -0600

    Refactor ol to become table w/ rows

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -42,22 +42,48 @@ var loadIdeas = $.getJSON('/api/v1/ideas').then(
   }
 )
 
+// var ideaFormatter = function(idea){
+//     var id      = idea.id;
+//     var title   = idea.title;
+//     var body    = idea.body.substring(0, 100) + ' ...';
+//     var quality = idea.quality;
+//
+//     // don't need div id
+//     var structure =
+//       '<h3>Title: ' + title + '</h3>' +
+//       '<h4>' + quality + '</h4>' +
+//       '<p>' + body + '</p>';
+//
+//     var deleteButton = '<button class="delete" id=' + id + '>Delete</button>';
+//
+//     $('.ideas').prepend(
+//       '<li id=' + id + '>' + structure + deleteButton + '</li>'
+//     );
+//
+//     $('#new_idea_title').val("");
+//     $('#new_idea_body').val("");
+//   }
+
+
+// jquery .delegate()
+// refactor / maybe pass function
+// content 'editable'
 var ideaFormatter = function(idea){
     var id      = idea.id;
     var title   = idea.title;
-    var body    = idea.body.substring(0, 100) + ' ...';
+    var body    = idea.body;
     var quality = idea.quality;
-
-    // don't need div id
-    var structure =
-      '<h3>Title: ' + title + '</h3>' +
-      '<h4>' + quality + '</h4>' +
-      '<p>' + body + '</p>';
 
     var deleteButton = '<button class="delete" id=' + id + '>Delete</button>';
 
-    $('.ideas').prepend(
-      '<li id=' + id + '>' + structure + deleteButton + '</li>'
+    var structure =
+      '<td>' + title + '</td>' +
+      '<td>' + quality + '</td>' +
+      '<td>' + body + '</td>' +
+      '<td>' + deleteButton + '</td>';
+
+    $('.ideas tr:first').after(
+      '<tr id=' + id + '>' + structure + '</tr>'
     );
 
     $('#new_idea_title').val("");
