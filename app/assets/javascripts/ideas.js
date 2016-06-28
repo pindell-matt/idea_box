5f26158daa918b1c5b3898ffd64f065b8bbba766
Matt Pindell <pindell.m@gmail.com>
2016-06-27 19:35:12 -0600

    Basic implementation of delete

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
-
-  $.getJSON('/api/v1/ideas').then(
-    function(ideasResponse, status){
-      $.each(ideasResponse, function(index, idea) {
-        ideaFormatter(idea);
-      });
-    }
-  )
+  loadIdeas
+    .then(function(){
+      deleteButtons();
+    })
 
   $('#idea-form').submit(function(event){
     event.preventDefault();
@@ -24,7 +20,17 @@ $(document).ready(function(){
     });
   });
 
-  function ideaFormatter(idea){
+});
+
+var loadIdeas = $.getJSON('/api/v1/ideas').then(
+  function(ideasResponse, status){
+    $.each(ideasResponse, function(index, idea) {
+      ideaFormatter(idea);
+    });
+  }
+)
+
+var ideaFormatter = function(idea){
     var title   = idea.title;
     var body    = idea.body.substring(0, 100) + ' ...';
     var quality = idea.quality;
@@ -34,14 +40,31 @@ $(document).ready(function(){
       '<h4>' + quality + '</h4>' +
       '<p>' + body + '</p>';
 
-    // var deleteButton = '<button id=' + title + '>Delete</button>';
+    var deleteButton = '<button id=' + idea.id + '>Delete</button>';
 
     $('.ideas').prepend(
-      '<li>' + structure + '</li>'
+      '<li>' + structure + deleteButton + '</li>'
     );
 
     $('#new_idea_title').val("");
     $('#new_idea_body').val("");
   }
 
-});
+var deleteButtons = function(){
+  $(':button').on('click', function(){
+    var data = this.id
+
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/v1/ideas/' + data,
+      data: data,
+      dataType: 'JSON',
+      success: function(data){
+        alert("TOTES WORKED, PLZ REFRESH");
+      },
+      error: function(data){
+        alert("FAILED TO DELETE");
+      }
+    });
+  })
+}
